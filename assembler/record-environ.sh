6e50ce350bd8bc4b919082611049d1e2154a3dd1
Jaeho Shin <netj@cs.stanford.edu>
2013-07-13 16:41:13 -0700

    Separated environment recording part

---
+++ assembler/record-environ.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+[ $# -gt 0 ] || { echo >&2 "Usage: record-environ.sh [NAME[=VALUE]]..."; exit 2; }
+for _3X_decl; do
+    _3X_name=${_3X_decl%%=*}
+    _3X_orig=_3X_ORIG_$_3X_name
+    if declare -p $_3X_orig &>/dev/null; then
+        echo "$_3X_name=${!_3X_orig}"
+    elif declare -p $_3X_name &>/dev/null; then
+        echo "$_3X_name=${!_3X_name}"
+    else
+        case $_3X_decl in
+            *=*) echo "$_3X_name=${_3X_decl#$_3X_name=}" ;;
+        esac
+    fi
+done
