a84677ff333594706bf2c8e900d39cb2383cfbc5
Jaeho Shin <netj@sparcs.org>
2013-10-03 17:31:31 -0700

    tutorial update script

---
+++ tutorial.update.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -eu
+cd "$(dirname "$0")"
+git rm -rf -- tutorial || true
+git read-tree --prefix=tutorial remotes/origin/master:docs/tutorial
+git checkout tutorial
+
+cd tutorial
+curl -X POST --data-urlencode content@README.md \
+    http://documentup.com/compiled \
+    >index.html
+git add index.html
+
+git commit -m "tutorial update"
+git push
