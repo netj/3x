58ba3d08c4a1b0a49df725f4e3f3f40b98c6b7fb
Jaeho Shin <netj@sparcs.org>
2013-10-04 07:53:44 -0700

    tutorial update

--- tutorial.update.sh
+++ tutorial.update.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 set -eu
 cd "$(dirname "$0")"
-git rm -rf -- tutorial || true
-git read-tree --prefix=tutorial remotes/origin/master:docs/tutorial
-git checkout tutorial
 
-cd tutorial
+git rm -rf -- tutorial || true
+git read-tree --prefix=docs/tutorial remotes/origin/master:docs/tutorial
+mkdir -p docs/tutorial
+(
+cd docs/tutorial
+git checkout README.md
 curl -X POST \
     --data name="3X Tutorial" \
     --data-urlencode content@README.md \
     http://documentup.com/compiled \
     >index.html
 git add index.html
+)
+
+git rm -rf -- docs/examples || true
+git read-tree --prefix=docs/examples remotes/origin/master:docs/examples
 
 git commit -m "tutorial update"
 git push
