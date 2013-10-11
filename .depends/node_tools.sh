0d5472937b16881aaa62a5adeeeb09b2a142a079
Jaeho Shin <netj@cs.stanford.edu>
2013-10-10 22:04:37 -0700

    Removed GNU coreutils dependency in node_tools dep
    which used cp's -l option. (thanks to Sudheendra Hangal)

--- .depends/node_tools.sh
+++ .depends/node_tools.sh
@@ -7,11 +7,12 @@ name=`basename "$0" .sh`
 
 cp -f ../"$name".json package.json
 rm -rf node_modules
+mkdir -p "$DEPENDS_PREFIX"/lib/node_modules
+ln -sfn "$DEPENDS_PREFIX"/lib/node_modules .
 date >README.md
 npm install
 
 mkdir -p "$DEPENDS_PREFIX"/bin
-cp -al node_modules "$DEPENDS_PREFIX"/lib/
 cd "$DEPENDS_PREFIX"
 for x in lib/node_modules/.bin/*; do
     relsymlink "$x" bin/
