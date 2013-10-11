8a5c4da9790fffb1e751cfc603ebaa3a9b223a74
Jaeho Shin <netj@cs.stanford.edu>
2013-10-11 00:04:39 -0700

    Polished GNU coreutils bundling script

--- depends/bundled/GNU-coreutils.sh
+++ depends/bundled/GNU-coreutils.sh
@@ -2,8 +2,6 @@
 # install GNU coreutils
 set -eu
 version=8.21
-sha1sum=3fba93c72fab62f3742fe50957d3a86d4cd08176
-md5sum=065ba41828644eca5dd8163446de5d64
 
 self=$0
 name=`basename "$0" .sh`
@@ -12,11 +10,10 @@ prefix="$(pwd -P)"/prefix
 
 # fetch source if necessary and prepare for build
 tarball=coreutils-${version}.tar.xz
-[ x"$(sha1sum <"$tarball" 2>/dev/null)" = x"$sha1sum  -" ] ||
-[ x"$(md5 <"$tarball" 2>/dev/null)" = x"$md5sum" ] ||
+[ -s "$tarball" ] ||
     curl -C- -RLO "http://ftp.gnu.org/gnu/coreutils/$tarball"
 xz -d <"$tarball" | tar xf -
-cd ./"coreutils-${version}"
+cd ./"${tarball%.tar*}"
 
 # configure and build
 ./configure --prefix="$prefix"
