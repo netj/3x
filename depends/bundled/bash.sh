2eb8963e0f4d1e91d7aaa3503255b9640c12335d
Jaeho Shin <netj@cs.stanford.edu>
2013-10-10 23:55:16 -0700

    Bundling bash 4.2.45(2)

---
+++ depends/bundled/bash.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# install Bash
+set -eu
+version=4.2
+patchesURL=https://gist.github.com/jacknagel/4008180/raw/1509a257060aa94e5349250306cce9eb884c837d/bash-4.2-001-045.patch
+
+self=$0
+name=`basename "$0" .sh`
+
+prefix="$(pwd -P)"/prefix
+
+# fetch source if necessary and prepare for build
+tarball=bash-${version}.tar.gz
+[ -s "$tarball" ] ||
+    curl -C- -RLO "http://ftpmirror.gnu.org/bash/$tarball"
+patchesName=${patchesURL##*/}
+[ -s "$patchesName" ] ||
+    curl -C- -RLOk "$patchesURL"
+tar xf "$tarball"
+cd ./"${tarball%.tar*}"
+patch -p0 <../"$patchesName"
+
+
+# configure and build
+./configure --prefix="$prefix"
+make -j $(nproc 2>/dev/null) install
+
+
+# place symlinks for commands to $DEPENDS_PREFIX/bin/
+mkdir -p "$DEPENDS_PREFIX"/bin
+for x in "$prefix"/bin/*; do
+    [ -x "$x" ] || continue
+    relsymlink "$x" "$DEPENDS_PREFIX"/bin/
+done
