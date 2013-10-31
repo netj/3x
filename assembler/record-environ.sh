fa227186c39157e6e2abb19315452344f04da3c8
Jaeho Shin <netj@cs.stanford.edu>
2013-10-31 02:03:14 -0700

    More handling of env vars with whitespaces

--- assembler/record-environ.sh
+++ assembler/record-environ.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
+escape() {
+    local arg=
+    for arg; do
+        echo "'"${arg//\'/\'\\\'\'}"'"
+    done
+}
 [ $# -gt 0 ] || { echo >&2 "Usage: record-environ.sh [NAME[=VALUE]]..."; exit 2; }
 for _3X_decl; do
     _3X_name=${_3X_decl%%=*}
     _3X_orig=_3X_ORIG_$_3X_name
     if declare -p -- $_3X_orig &>/dev/null; then
-        echo "$_3X_name=${!_3X_orig}"
+        escape "$_3X_name=${!_3X_orig}"
     elif declare -p -- $_3X_name &>/dev/null; then
-        echo "$_3X_name=${!_3X_name}"
+        escape "$_3X_name=${!_3X_name}"
     else
         case $_3X_decl in
-            *=*) echo "$_3X_name=${_3X_decl#$_3X_name=}" ;;
+            *=*) escape "$_3X_name=${_3X_decl#$_3X_name=}" ;;
         esac
     fi
 done
