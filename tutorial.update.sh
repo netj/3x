07be39e0a18c8915d5bfdc2565c7ca103154ec74
Jaeho Shin <netj@sparcs.org>
2013-10-31 16:48:33 -0700

    Always fetch origin when updating tutorial

--- tutorial.update.sh
+++ tutorial.update.sh
@@ -2,6 +2,7 @@
 set -eu
 cd "$(dirname "$0")"
 
+git fetch origin
 git rm -rf -- docs/tutorial || true
 git read-tree --prefix=docs/tutorial remotes/origin/master:docs/tutorial
 mkdir -p docs/tutorial
