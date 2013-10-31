8240af452c61d06e35fd953cfff0e4899c4d1e5e
Jaeho Shin <netj@sparcs.org>
2013-10-31 15:53:57 -0700

    Fixed nav bar for Tutorial

--- tutorial.update.sh
+++ tutorial.update.sh
@@ -13,11 +13,21 @@ curl -X POST \
     --data-urlencode content@README.md \
     http://documentup.com/compiled \
     >index.html
+    # polish DocumentUp's style
+    {
+        echo '/<\/head/i'
+        echo '<link rel="stylesheet" href="../../nav-fix.css">'
+        echo '.'
+        echo 'wq'
+    } | ed index.html
 git add index.html
 )
 
 git rm -rf -- docs/examples || true
 git read-tree --prefix=docs/examples remotes/origin/master:docs/examples
+
+# confirm publishing
+read -p "commit and push? "
 
 git commit -m "tutorial update"
 git push
