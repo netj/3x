f49f970fd9730304ae9fb6399dabf8fe569b9deb
Jaeho Shin <netj@sparcs.org>
2013-11-21 18:53:00 -0800

    footer + nav.footer with common links

--- update.sh
+++ update.sh
@@ -6,9 +6,28 @@ DocumentUpOptions+=(
     google_analytics="UA-29293848-3"
 )
 
-insert-nav-extras() {
-    echo '<div class="extra"><a href="http://netj.github.io/3x"><i class="icon-beaker"></i> 3X Home</a></div>'
+#insert-footer() { echo '<address>&copy; 2013 <a href="http://infolab.stanford.edu">InfoLab, Stanford University</a>.</address>'; }
+
+
+common-links() {
+    echo '<div class="extra"><a href="https://github.com/'"$GitHubRepo"'"><i class="fa fa-github-alt"></i> Source Code</a></div>'
+    echo '<div class="extra"><a href="https://github.com/'"$GitHubRepo"'/issues"><i class="fa fa-bug"></i> Issues</a></div>'
 }
+link() {
+    local title=$1 path=$2
+    echo '<div class="extra"><a href="http://'"${GitHubRepo%/*}"'.github.io/'"${GitHubRepo#*/}"'/'"$path"'"><i class="fa fa-flask"></i> '"$title"'</a></div>'
+}
+
+insert-nav-extras() {
+    local self=$1
+    link-no-self() { [[ $2 = $self ]] || link "$@"; }
+    link-no-self "3X"           "."
+    link-no-self "Features"     "docs/features"
+    link-no-self "Installation" "docs/install"
+    link-no-self "Tutorial"     "docs/tutorial"
+    common-links
+}
+
 
 # README preview
 mirror-master .
@@ -18,12 +37,12 @@ compile-markdown . name="3X" #"3X for eXecuting eXploratory eXperiments"
 mirror-master docs/features
 compile-markdown docs/features name="3X Features"
 
+# Installation
+mirror-master docs/install
+compile-markdown docs/install name="3X Installation"
+
 # Tutorial
 mirror-master docs/tutorial
 compile-markdown docs/tutorial name="3X Tutorial"
 # and examples
 mirror-master docs/examples
-
-# Installation
-mirror-master docs/install
-compile-markdown docs/install name="3X Installation"
